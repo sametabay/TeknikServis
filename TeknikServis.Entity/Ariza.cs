979aca4d07199d7220d14beb62b3c6617ece37af
sametabay <sametabay37@hotmail.com>
2016-11-07 23:02:55 +0200

    07.11.2016

---
+++ TeknikServis.Entity/Ariza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknikServis.Entity
+{
+    [Table("Arizalar")]
+   public class Ariza
+    {
+        [Key]
+        public int ArizaID { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public string Adres { get; set; }
+        public string Rapor { get; set; }
+        public string FotografYolu { get; set; }
+        public DateTime EklenmeTarihi { get; set; } = DateTime.Now;
+        public bool OnaylandiMi { get; set; } = false;
+        public DateTime OnaylanmaTarihi { get; set; }
+
+    }
+}
